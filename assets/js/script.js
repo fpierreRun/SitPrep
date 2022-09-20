b538f5ddd8066b2efb7a03287ece7b83f530c8e2
Dione Pierre-Louis <francisd.plouis@gmail.com>
2022-09-20 03:09:36 -0600

    added hide buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,28 @@
 
 //     console.log(Math.max(0, 25, -5));
 //     console.log(Math.floor(Math.random() * 60)+10);
+
+
+
+// next1.addEventListener("submit", submitOne);
+// locationForm.addEventListener("submit", submitOne);
+// locationForm.addEventListener("click", stepTwo);
+
     
+// function stepTwo() {
+//     // Set items for page
+//     alert("button clicked");
+   
+        
+//         document.getElementById("foodForm").style.display = "block";
+//         console.log()
+
+// }
+
+function submitOne() {
+    document.getElementById("step1").style.display = "none";
+    document.getElementById("foodForm").style.display = "block";
+    // Set items for page
+    // alert("Form submitted");
+    //  console.log(submitOne);
+}
