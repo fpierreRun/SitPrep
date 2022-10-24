54c5780349e15166bdd9c6045a3698ddc8f203f9
Dione Pierre-Louis <francisd.plouis@gmail.com>
2022-10-23 22:10:44 -0600

    fixing PDF bug

--- assets/js/load.js
+++ assets/js/load.js
@@ -267,44 +267,44 @@ var updateEdited = function () {
 
 
 
-    //download PDF code
+     //download PDF code
 
-    $(document).ready(function($) 
-	{ 
+     $(document).ready(function($) 
+     { 
+   
+       $(document).on('click', '.btn_print', function(event) 
+       {
+         event.preventDefault();
+   
+         //credit : https://ekoopmans.github.io/html2pdf.js
+         
+         var element = document.getElementById('downloadPlan'); 
+   
+         
+   
+         //more custom settings
+         var opt = 
+         {
+           margin:     0.25,
+           filename:     'My_Plan_.pdf',
+           image:        { type: 'pdf', quality: 0.98 },
+           html2canvas:  { scale: 2 },
+           jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+         };
+   
+         // New Promise-based usage:
+         html2pdf().set(opt).from(element).save();
+   
+         // Avoid page-breaks on all elements, and add one before #page2el.
+           html2pdf().set({
+             pagebreak: { mode: 'avoid-all', before: '#page2el' }
+           });
+   
+       });
+   
+     });
+  //download PDF code ends
 
-		$(document).on('click', '.btn_print', function(event) 
-		{
-			event.preventDefault();
-
-			//credit : https://ekoopmans.github.io/html2pdf.js
-			
-			var element = document.getElementById('downloadPlan'); 
-
-			
-
-			//more custom settings
-			var opt = 
-			{
-			  margin:     0.25,
-			  filename:     'My_Plan_.pdf',
-			  image:        { type: 'pdf', quality: 0.98 },
-			  html2canvas:  { scale: 2 },
-			  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-			};
-
-			// New Promise-based usage:
-			html2pdf().set(opt).from(element).save();
-
-      // Avoid page-breaks on all elements, and add one before #page2el.
-        html2pdf().set({
-          pagebreak: { mode: 'avoid-all', before: '#page2el' }
-        });
-
-		});
-
- 
- 
-	});
 
   var saveData = function () {
     localStorage.setItem("editedPlan", JSON.stringify(editedPlan))
