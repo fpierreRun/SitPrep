5551756b0d9b85d291e13c9a635205d8e219a144
Dione Pierre-Louis <francisd.plouis@gmail.com>
2022-10-25 01:03:30 -0600

    add script and html for foodstorage calc

---
+++ assets/js/calc.js
@@ -0,0 +1,14 @@
+
+
+
+function planChild() {
+    
+
+    if(addSecond.checked==true) {
+        document.getElementById("planChild").style.display = "block"
+    ;}
+
+    else{
+        document.getElementById("planChild").style.display = "none";
+   ;}
+;}
