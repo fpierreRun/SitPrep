909e29664821413652ae32df01de4bc0cdc6dcd7
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-07-04 15:41:38 -0600

    updated code for bugs

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,43 +1,42 @@
+let map, directionsService, directionsRenderer;
+
 function initMap() {
-    const directionsRenderer = new google.maps.DirectionsRenderer();
-    const directionsService = new google.maps.DirectionsService();
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 7,
-      center: { lat: 41.85, lng: -87.65 },
-      disableDefaultUI: true,
-    });
-  
-    directionsRenderer.setMap(map);
-    document.getElementById("mode").addEventListener("change", () => {
-        calculateAndDisplayRoute(directionsService, directionsRenderer)});
-    directionsRenderer.setPanel(document.getElementById("sidebar"));
-  
-    const control = document.getElementById("floating-panel");
-  
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-  
-    const onChangeHandler = function () {
-      calculateAndDisplayRoute(directionsService, directionsRenderer);
-    };
-  
-    document.getElementById("start").addEventListener("change", onChangeHandler);
-    document.getElementById("end").addEventListener("change", onChangeHandler);
-  }
-  
-  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    const start = document.getElementById("start").value;
-    const end = document.getElementById("end").value;
-  
-    directionsService
-      .route({
-        origin: start,
-        destination: end,
-        travelMode: google.maps.TravelMode[selectedMode],
-      })
-      .then((response) => {
+  directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: { lat: 41.85, lng: -87.65 },
+    disableDefaultUI: true,
+  });
+
+  directionsRenderer.setMap(map);
+  document.getElementById("mode").addEventListener("change", calculateAndDisplayRoute);
+  directionsRenderer.setPanel(document.getElementById("sidebar"));
+
+  const control = document.getElementById("floating-panel");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+  document.getElementById("start").addEventListener("change", calculateAndDisplayRoute);
+  document.getElementById("end").addEventListener("change", calculateAndDisplayRoute);
+}
+
+function calculateAndDisplayRoute() {
+  const start = document.getElementById("start").value;
+  const end = document.getElementById("end").value;
+  const selectedMode = document.getElementById("mode").value;
+
+  directionsService.route(
+    {
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode[selectedMode],
+    },
+    (response, status) => {
+      if (status === "OK") {
         directionsRenderer.setDirections(response);
-      })
-      .catch((e) => window.alert("Directions request failed due to " + error));
-  }
-  
-  window.initMap = initMap;
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
+}
