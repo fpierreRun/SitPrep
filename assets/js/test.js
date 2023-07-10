6f56b85e2672d73000ca2893f245ffa0d49c1ad6
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-07-10 05:51:32 -0600

    adding email sharing

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,43 +1,20 @@
-// Add a click event listener to the "Share via Email" button
-document.querySelector('#shareViaEmail').addEventListener('click', function() {
-  // Select the element you want to convert to PDF (in this case, the whole page)
-  const element = document.documentElement;
+document.getElementById("shareViaEmail").addEventListener("click", function() {
+  // Generate the PDF using html2pdf.js
+  const element = document.getElementById("downloadPlan");
+  html2pdf()
+    .set({ filename: 'evacuation_plan.pdf', image: { type: 'jpeg', quality: 0.98 } })
+    .from(element)
+    .save();
 
-  // Set the options for PDF generation
-  const options = {
-    margin: 10,
-    filename: 'evacuation_plan.pdf',
-    image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-  };
+  // Send the email with the PDF attachment using PHP
+  const formData = new FormData();
+  formData.append('pdf', element);
 
-  // Use html2pdf library to generate the PDF
-  html2pdf().set(options).from(element).outputPdf('datauristring').then(function(pdfDataUri) {
-    // Create a temporary link to the generated PDF
-    const link = document.createElement('a');
-    link.href = pdfDataUri;
-
-    // Set the link to open in a new tab
-    link.target = '_blank';
-
-    // Add the download attribute and set the file name
-    link.download = 'evacuation_plan.pdf';
-
-    // Simulate a click on the link to trigger the download
-    link.click();
-
-    // Encode the PDF data URI
-    const encodedUri = encodeURIComponent(pdfDataUri);
-
-    // Create the email subject and body
-    const subject = 'Evacuation Plan';
-    const body = 'Check out my evacuation plan!';
-
-    // Create a mailto link with the encoded PDF data URI, subject, and body
-    const mailtoLink = `mailto:?subject=${subject}&body=${body}%0D%0A%0D%0A${encodedUri}`;
-
-    // Open the mailto link in a new window or redirect to the default email client
-    window.open(mailtoLink, '_blank');
-  });
+  fetch('send_email.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
 });
