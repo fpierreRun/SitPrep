c8d027aa02139a368db22f569e9e1eba2955ea72
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-08-08 12:21:43 -0600

    fix doaload mobile issues

--- assets/js/form.js
+++ assets/js/form.js
@@ -186,76 +186,6 @@ window.location.href="./dashboard Main.html";
 
 
 
-// //function for address field
-
-// "use strict";
-
-// function initMap() {
-//   const CONFIGURATION = {
-//     "ctaTitle": "Checkout",
-//     "mapOptions": {"center":{"lat":37.4221,"lng":-122.0841},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":true,"zoom":11,"zoomControl":false,"maxZoom":22,"mapId":""},
-//     "mapsApiKey": "AIzaSyDZ94vMQMhdMLFDd3LbpcSrjPK6rPT-3WA",
-//     "capabilities": {"addressAutocompleteControl":true,"mapDisplayControl":false,"ctaControl":false}
-//   };
-//   const componentForm = [
-//     'location',
-//     'locality',
-//     'administrative_area_level_1',
-//     'country',
-//     'postal_code',
-//   ];
-
-
-//   const getFormInputElement = (component) => document.getElementById(component + '-input2');
-//   const autocompleteInput = getFormInputElement('location');
-//   const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
-//     fields: ["address_components", "geometry", "name"],
-//     types: ["address"],
-//   });
-
-//   autocomplete.addListener('place_changed', function () {
-//     const place = autocomplete.getPlace();
-//     if (!place.geometry) {
-//       // User entered the name of a Place that was not suggested and
-//       // pressed the Enter key, or the Place Details request failed.
-//       window.alert('No details available for input: \'' + place.name + '\'');
-//       return;
-//     }
-//     fillInAddress(place);
-    
-//   });
-
-//   function fillInAddress(place) {  // optional parameter
-//     const addressNameFormat = {
-//       'street_number': 'long_name',
-//       'route': 'long_name',
-//       'locality': 'long_name',
-//       'administrative_area_level_1': 'long_name',
-//       'country': 'long_name',
-//       'postal_code': 'long_name',
-//     };
-
-
-//     const getAddressComp = function (type) {
-//       for (const component of place.address_components) {
-//         if (component.types[0] === type) {
-//           return component[addressNameFormat[type]];
-//         }
-//       }
-//       return '';
-//     };
-
-//     getFormInputElement('location').value = getAddressComp('street_number') + ' '
-//               + getAddressComp('route');
-//         for (const component of componentForm) {
-//           // Location field is handled separately above as it has different logic.
-//           if (component !== 'location') {
-//             getFormInputElement(component).value = getAddressComp(component);
-//       }
-//     }
-//   }
-// };
-//end of cuntion for address field
 
 
 //beggining of save data in local storage
@@ -288,10 +218,10 @@ function downloadAsPDF() {
 
   // Provide options for PDF generation
   const options = {
-    margin: [5, 5, 5, 5], // Adjust the margins as per your requirement
+    margin: [8, 8, 8, 8], // Adjust the margins as per your requirement
     filename: 'My_Evacuation_plan.pdf',
     image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2, useCORS: true, scrollY: 0   },
+    html2canvas: { scale: 3, useCORS: true, scrollY: 0,  },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
   };
 
@@ -303,8 +233,6 @@ function downloadAsPDF() {
     pagebreak: { mode: 'avoid-all', after: '#page2el' }
   });
 
- 
- 
 }
 
 // Call the downloadAsPDF function when the button is clicked
@@ -320,9 +248,6 @@ function showPlan(element) {
   document.getElementById("editVtwo").hidden = true;
  
 
-  // document.getElementById("editVthree").hidden = true;
-  // document.getElementById("editVthree").hidden = true;
-  // document.getElementById("editVthree").hidden = true;
 
   document.getElementById("pdfVone").removeAttribute("hidden");
   document.getElementById("pdfVtwo").removeAttribute("hidden");
@@ -353,49 +278,27 @@ function showPlan(element) {
 
 }
 
-// function showPlan(element) {
-//   const editVersions = document.querySelectorAll('[id^="editVersion"]');
-//   editVersions.forEach((editVersion) => {
-//     editVersion.style.display = "none";
-//   });
-
-//   document.getElementById("step1").style.display = "block";
-//   document.getElementById("evacForm").style.display = "block";
-//   document.getElementById("contactForm").style.display = "block";
-//   document.getElementById("downloadpdf1").style.display = "block";
-//   document.getElementById("editPdf").style.display = "block";
-
-//   const elementsToHide = element.querySelectorAll('.element-to-hide');
-//   elementsToHide.forEach((el) => {
-//     el.setAttribute('hidden', true);
-//   });
-// }
 
 
 function editPlan(element) {
  // Modify the querySelectorAll call to select the element by its id
 document.getElementById("editVone").removeAttribute("hidden");
 document.getElementById("editVtwo").removeAttribute("hidden");
-// document.getElementById("editVthree").removeAttribute("hidden");
 
-// document.getElementById("editVthree").hidden = true;
-// document.getElementById("editVthree").hidden = true;
-// document.getElementById("editVthree").hidden = true;
 
 document.getElementById("pdfVone").hidden = true;
 document.getElementById("pdfVtwo").hidden = true;
 document.getElementById("previous1").hidden = true;
-// document.getElementById("downloadPlan2").hidden = true;
+
 document.getElementById("titleSig").hidden = true;
 
-// document.getElementById("removeMap5Button").removeAttribute("hidden");
+
 document.getElementById("removeMap2Button").removeAttribute("hidden");
 document.getElementById("removeMap3Button").removeAttribute("hidden");
 document.getElementById("addSecLocButton").removeAttribute("hidden");
 document.getElementById("addMap2Button").removeAttribute("hidden");
 document.getElementById("addMap3Button").removeAttribute("hidden");
-// document.getElementById("save").removeAttribute("hidden");
-// document.getElementById("previous2").hidden = true;
+
 
 
 
