d46d0e3b63f1dafb507303b65e5cc8a466f3f7e2
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-08-17 20:44:45 -0600

    added

--- public/script.js
+++ public/script.js
@@ -28,20 +28,19 @@ function askQuestion() {
 function formatResponse(response) {
     const responseLines = response.split('\n');
 
-    // Process each line to identify and format Amazon links
+    // Process each line to identify and format links as bolded hyperlinks
     const formattedLines = responseLines.map(line => {
-        const amazonLinkRegex = /\[([^\]]+)\]\((https?:\/\/[^\s\)]+)\)/g; // Regex to match [title](link)
+        const linkRegex = /\[([^\]]+)\]\((https?:\/\/[^\s\)]+)\)/g; // Regex to match [title](link)
 
-        const formattedLine = line.replace(amazonLinkRegex, (_, title, link) => {
-            return `<p><strong>${title}:</strong> <a href="${link}" target="_blank">${link}</a></p>`;
+        const formattedLine = line.replace(linkRegex, (_, title, url) => {
+            return `<a href="${url}" target="_blank"><strong>${title}</strong></a>`;
         });
 
-        return formattedLine;
+        return `<p>${formattedLine}</p>`;
     });
 
     return formattedLines.join('');
 }
-
 
 
 
