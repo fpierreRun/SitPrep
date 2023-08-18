48c6eeeed81e218e93dbead99577d3d3f67c6791
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-08-17 21:11:18 -0600

    done

--- public/script.js
+++ public/script.js
@@ -31,14 +31,14 @@ function formatResponse(response) {
     // Process each line to identify and format links and specified text as bolded
     const formattedLines = responseLines.map(line => {
         const linkRegex = /\[(.*?)\]\((https?:\/\/[^\s]+)\)/g; // Regex to match [Title](URL) format
-        const boldTextRegex = /(\d+\.)\s+([^\n]+)/g; // Regex to match specified format: 1. Text
+        const boldTextRegex = /(\d+\.)\s+([^\n]+:)/g; // Regex to match specified format: 1. Text:
 
         // Format links as bolded hyperlinks
         const formattedLine = line.replace(linkRegex, (match, title, url) => {
             return `<a href="${url}" target="_blank"><strong>${title}</strong></a>`;
         });
 
-        // Bold specified format: 1. Text
+        // Bold specified format: 1. Text:
         const finalFormattedLine = formattedLine.replace(boldTextRegex, (match, number, text) => {
             return `<strong>${number} ${text}</strong>`;
         });
@@ -48,9 +48,6 @@ function formatResponse(response) {
 
     return formattedLines.join('');
 }
-
-
-
 
 
 document.getElementById('submitBtn').addEventListener('click', askQuestion);
