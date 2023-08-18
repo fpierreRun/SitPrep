ff8a16a55b5aa7a452c26a77eff4487b811bc7c0
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-08-18 01:16:13 -0600

    reverted

--- server.js
+++ server.js
@@ -41,25 +41,15 @@ app.post('/ask', async (req, res) => {
       
     ];
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: systemMessage,
-      temperature: 1,
-      max_tokens: 3796,
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0,
+      max_tokens: 1000,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     });
-
-    // const response = await openai.createChatCompletion({
-    //   model: "gpt-3.5-turbo",
-    //   messages: messages,
-    //   temperature: 0,
-    //   max_tokens: 1000,
-    //   top_p: 1,
-    //   frequency_penalty: 0,
-    //   presence_penalty: 0,
-    // });
 
     let answer = response.data.choices[0].message.content;
 
