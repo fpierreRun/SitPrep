0f35104b4571dfeeb7d46a059bb3631f49ab9fc2
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-09-06 14:06:32 -0600

    changed text color

--- public/script.js
+++ public/script.js
@@ -38,7 +38,7 @@ function formatResponse(response) {
         const formattedContent = message.role === 'assistant' ? formatAssistantResponse(message.content) : message.content;
 
         if (message.role === 'assistant') {
-            return `<div class="sai-response txtBlue px-3"><strong>${role}: </strong>${formattedContent}</div><br>`;
+            return `<div class="sai-response text-secondary px-3"><strong>${role}: </strong>${formattedContent}</div><br>`;
         } else {
             return `<div class="chat-message px-3"><strong>${role}: </strong>${formattedContent}</div><br>`;
         }
