b9571fce8d89a382ec5c3af3e925108af6d4e848
Dione Pierre-Louis <dione@diones-mbp.lan>
2023-09-06 14:11:34 -0600

    added grey text

--- public/script.js
+++ public/script.js
@@ -38,7 +38,7 @@ function formatResponse(response) {
         const formattedContent = message.role === 'assistant' ? formatAssistantResponse(message.content) : message.content;
 
         if (message.role === 'assistant') {
-            return `<div class="sai-response text-secondary px-3"><strong>${role}: </strong>${formattedContent}</div><br>`;
+            return `<div style="color: #3e3e3e; class="sai-response px-3"><strong>${role}: </strong>${formattedContent}</div><br>`;
         } else {
             return `<div class="chat-message px-3"><strong>${role}: </strong>${formattedContent}</div><br>`;
         }
